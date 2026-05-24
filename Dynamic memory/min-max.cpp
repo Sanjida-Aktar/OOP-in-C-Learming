a83877022b6cd6714a79135a3e5ef25b3338319c
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-24 23:28:13 +0600

    add min max

---
+++ Dynamic memory/min-max.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+        cout<<"Enter the number of elements: "; 
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cout<<"Enter element "<<i+1<<": ";
+            cin>>arr[i];
+        }
+        int min=arr[0];
+        int max=arr[0];
+        for(int i=1; i<n; i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        cout<<"Minimum element: "<<min<<endl;
+        cout<<"Maximum element: "<<max<<endl;
+    return 0;
+}
