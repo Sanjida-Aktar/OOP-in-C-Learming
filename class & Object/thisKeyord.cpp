d47008308a058f372b761b248575205e0646ea83
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-26 23:19:14 +0600

    add constractor and use this keyword

---
+++ class & Object/thisKeyord.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int age;
+
+    Student(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+};
+
+int main(){
+    Student s1("Alice", 20);
+    s1.display();
+    return 0;
+}
