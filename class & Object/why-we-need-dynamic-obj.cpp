f99a7716a70f3111b235d47336ad5d0a078610e2
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-26 23:40:40 +0600

    why we need dynamic object

---
+++ class & Object/why-we-need-dynamic-obj.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int age;
+
+    Student(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+    
+};
+Student* fun(){
+    Student s("Bob", 25);
+    Student *p = &s; // Pointer to the local object
+    return p; // Returning the pointer to the object
+}
+int main(){
+    Student *s2 = fun();
+    cout<<"Name: "<<s2->name<<endl;
+    cout<<"Age: "<<s2->age<<endl;
+    return 0;
+}
