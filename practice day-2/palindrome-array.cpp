8286616b668a074651bf71a77917ebea68038538
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-28 23:41:53 +0600

    add check palindrome array

---
+++ practice day-2/palindrome-array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bool isPalindrome = true;
+    for(int i=0;i<n/2;i++){
+        if(arr[i] != arr[n-1-i]){
+            isPalindrome = false;
+            break;
+        }
+    }
+    if(isPalindrome){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
