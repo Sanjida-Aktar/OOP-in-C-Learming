38155a8f92ef541e2b09d7da8d75dc3a6eca298c
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-29 21:56:15 +0600

    add smallest pair sum

--- practice day-2/smallest-pair.cpp
+++ practice day-2/smallest-pair.cpp
@@ -1,6 +1,21 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-     
+   int t,n;
+   cin>>t;
+   while(t--){
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int min_num=INT_MAX;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n;j++){
+            min_num=min(min_num, arr[i]+arr[j]+j-i);
+        }
+    }
+    cout<<min_num;
+   }  
     return 0;
 }
