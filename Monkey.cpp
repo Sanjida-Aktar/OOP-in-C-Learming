4a325f8963277b111253097524d526abeba032a9
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-30 23:09:52 +0600

    monkey

---
+++ Monkey.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+     while(cin >> s && !cin.eof())
+     
+     {
+        char ch[]={};
+        sort(s.begin(),s.end());
+        cout<<s;
+     }
+
+    return 0;
+}
