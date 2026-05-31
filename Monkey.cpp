2f20815a8a57fd0b9c2ef51be7b127cfd7bb1292
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-31 22:28:27 +0600

    add monkey

--- Monkey.cpp
+++ Monkey.cpp
@@ -1,13 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    string s;
-     while(cin >> s && !cin.eof())
-     
-     {
-        sort(s.begin(),s.end());
-        cout<<s;
-     }
 
+int main() {
+    char s[100000+5];  
+    while (cin.getline(s, 100000)) {
+        int len = strlen(s);
+
+        int j = 0;
+        for (int i = 0; i < len; i++) {
+            if (s[i] != ' ') {   
+                s[j++] = s[i];
+            }
+        }
+        s[j] = '\0'; 
+
+        sort(s, s + strlen(s));
+
+        cout << s << endl;
+    }
     return 0;
 }
