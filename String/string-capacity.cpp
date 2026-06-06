54410063b9124d1dc9a009a316414c2b7d09b56d
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-06-06 15:43:49 +0600

    add string capacity

--- String/string-capacity.cpp
+++ String/string-capacity.cpp
@@ -3,5 +3,19 @@ using namespace std;
 int main(){
      string s="hello world";
      cout<<s.size()<<endl;
+     cout<<s.max_size()<<endl;
+     cout<<s.capacity()<<endl;
+     s.clear();
+     if(s.empty()){
+        cout<<"String is empty."<<endl;
+     }
+     else{
+        cout<<"String is not empty."<<endl;
+     }
+     string s1="hello";
+     s1.resize(3);
+     cout<<s1<<endl;
+     s1.resize(10,'x');
+     cout<<s1<<endl;
     return 0;
 }
