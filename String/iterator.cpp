3b1da8221555799aa69116089cba36e27988fce0
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-06-08 23:51:03 +0600

    add looping iterator

--- String/iterator.cpp
+++ String/iterator.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-     
+     string s="hello world";
+    //  for(int i=0;i<s.size();i++){
+    //     cout<<s[i]<<" ";
+    //  }
+        for(auto it=s.begin();it!=s.end();it++){
+            cout<<*it<<" ";
+        }
+        cout<<endl;
+        for(auto it=s.rbegin();it!=s.rend();it++){
+            cout<<*it<<" ";
+        }
+        cout<<endl;
     return 0;
 }
