8220f74854177df29bfff6435cb82c1bb5493470
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-06-15 17:28:46 +0600

    add sort array of object

---
+++ String/sort-array-of-object.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int marks;
+    int roll;
+    Student(string name,int roll,int marks){
+
+        this->name=name;
+        this->roll=roll;
+        this->marks=marks;
+    }
+};
+bool cmp(Student a,Student b){
+    // if(a.marks==b.marks){
+    //     return a.name<b.name;
+    // }
+    // else{
+    //     return a.marks>b.marks;
+    // }
+    return a.marks==b.marks ? a.roll<b.roll : a.marks>b.marks;
+}
+int main(){
+     int n;
+     cin>>n;
+     Student s[n];
+        for(int i=0;i<n;i++){
+            cin>>s[i].name>>s[i].roll>>s[i].marks;
+        }
+        sort(s,s+n,cmp);
+        for(int i=0;i<n;i++){
+            cout<<s[i].name<<" "<<s[i].roll<<" "<<s[i].marks<<endl;
+        }
+    return 0;
+}
